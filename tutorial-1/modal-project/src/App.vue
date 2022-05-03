937f0a553952f0be09c96207e905d8036087a0ef
erhanask <erhanasik8@gmail.com>
2022-05-03 13:38:06 +0300

    modal project finished.

--- tutorial-1/modal-project/src/App.vue
+++ tutorial-1/modal-project/src/App.vue
@@ -1,38 +1,75 @@
 <template>
-
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">Click me</button>
-  <Modal/>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">open modal</button>
+
+  <br>
+  <br>
+  <br>
+
+  <h1>{{ title2 }}</h1>
+  <p>Toggle Modal 2...</p>
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">Buy</a>
+        <a href="#">Loan</a>
+      </template>
+      <h1>Things of your dream!</h1>
+      <p>Buy or loan now!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal2">open modal 2</button>
+
+
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
-
+import Modal from './components/Modal'
 export default {
   name: 'App',
-  components: {Modal},
+  components: { Modal },
   data() {
     return {
-      title : 'Welcome To Site !',
+      title: 'My First Vue App!',
+      title2: 'Second Modal!',
+      showModal: false,
+      showModal2: false,
     }
   },
   methods: {
-    handleClick(){
-      console.log(this.$refs.name.value)
-      this.$refs.name.classList.add('eee');
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app , .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
