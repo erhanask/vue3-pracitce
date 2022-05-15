13134a02aa2dd803a5d809bae18c8233c56920f8
erhanask <erhanasik8@gmail.com>
2022-05-15 11:46:15 +0300

    Added json server and fetched datas from db.json file.

--- tutorial-1/router-project/src/views/jobs/JobsDeatilView.vue
+++ tutorial-1/router-project/src/views/jobs/JobsDeatilView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <h1>This is Jobs Detail page </h1>
-    <div>
-        {{id}}
+    <div v-if="job">
+        {{id}}. Jobs title is {{job.title}} and detail is {{ job.details }}
+    </div>
+    <div v-else>
+        Cant find data.
     </div>
   </div>
 </template>
@@ -13,6 +16,16 @@
 export default {
   name: 'JobsDetailView',
   props: ['id'],
-  components: {}
+  data(){
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/'+this.id)
+        .then(res => res.json())
+        .then(data => this.job= data)
+        .catch(err => console.log(err.message));
+  }
 }
 </script>
